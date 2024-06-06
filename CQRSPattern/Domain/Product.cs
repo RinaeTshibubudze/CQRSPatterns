eb1c54fd4e874d52a7f99306d51c456236bf8410
Rinae <vharhee@gmail.com>
2024-06-06 14:22:04 +0200

    Created the domain and the Persistence folders

---
+++ CQRSPattern/Domain/Product.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CQRSPattern.Domain
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = default!;
+        public string Description { get; set; } = default!;
+        public decimal Price { get; set; }
+
+        // Parameterless constructor for EF Core
+        private Product() { }
+        public Product(string name, string description, decimal price)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            Description = description;
+            Price = price;
+        }
+    }
+}
